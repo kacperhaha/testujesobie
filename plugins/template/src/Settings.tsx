d00c9fd47c58f496f902778b31fb96345327c398
Kacperhaha <kacperhaha11@gmail.com>
2023-07-18 18:21:14 +0200

    testowanko

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,23 @@
 import { Forms } from "@vendetta/ui/components";
+import { storage } from "@vendetta/plugin";
 const { FormText } = Forms;
+import { useProxy } from "@vendetta/storage";
 
-export default () => (
-    <FormText>
-        Hello, world test321!
-    </FormText>
+export default () => {
+    const [format, setFormat] = React.useState(storage.delay);
+
+useProxy(storage);
+return (
+    <FormInput
+       value={format}
+       onChange={v => {
+          storage.format = v == "" ? null : v
+          setFormat(format)
+       }}
+       placeholder={"chuj"}
+       title='Custom Timestamp Format'
+    />
+
+
 )
+    }
--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,13 +1,8 @@
-import { after, before } from '@vendetta/patcher';
-import { ReactNative } from '@vendetta/metro/common';
+import { after} from '@vendetta/patcher';
 import { storage } from '@vendetta/plugin';
 import {findByName, findByStoreName} from "@vendetta/metro";
 import Settings from "./Settings";
-const { DCDChatManager } = ReactNative.NativeModules;
-
 const RowManager = findByName("RowManager");
-const MessageStore = findByStoreName("MessageStore");
-let unpatchUpdateRows;
 let unpatch: Function;
 function snowflakeToDate(snowflake) {
     const dateBits = Number(BigInt.asUintN(64, snowflake) >> 22n);
@@ -40,41 +35,15 @@ export const onLoad = () => {
     //     message.referencedMessage.message.timestamp = `${snowflakeToDate(message.referencedMessage.message.id).toLocaleString()}`
     //   }
   
-    // message.timestamp = `${message.timestamp} - ${UserStore.get(message.authorId)?.username}`
 
     
 
 
   });
-
-// unpatchUpdateRows = before("updateRows", DCDChatManager, args => {
-//     const rows = JSON.parse(args[1]);
-
-//     for ( const row of rows ) {
-//         if (row.rowType !== 1) continue;
-
-
-//         if (row.message.timestamp) {
-//             row.message.timestamp = `${snowflakeToDate(row.message.id).toLocaleString()}`
-//             continue;
-//         }
-
-//     }
-
-//     /**
-//      * Finally, re-stringify the row.
-//      */
-//     args[1] = JSON.stringify(rows);
-// });
-
-
-
-
 };
 
 export const onUnload = () => {
     unpatch?.();
-    unpatchUpdateRows?.();
   };
 
 export const settings = Settings;
